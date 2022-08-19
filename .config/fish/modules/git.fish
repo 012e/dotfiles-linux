0aae8b1db5e66b852361942c88cf2cc9add5a923
012e <huyphmnhat@gmail.com>
2022-08-19 16:09:49 +0700

    add new git alias

--- .config/fish/modules/git.fish
+++ .config/fish/modules/git.fish
@@ -3,7 +3,15 @@ alias g="git"
 alias gc="git commit"
 alias gl="git log --graph --abbrev-commit --decorate --date=relative -10 --format=format:'%C(bold blue)%h%C(reset) %C(bold yellow)%d%C(reset) %C(white)%s%C(reset) %C(green)(%ar)%C(reset) %C(dim white)- %an%C(reset)'"
 alias gp="git push"
-alias gb="git checkout -b"
 alias gs="git status"
+alias gd="git diff"
+
+function gb
+    echo "Check if branch "$argv" exist"
+    if not git checkout $argv
+        echo "$argv not exist! Creating new branch."
+        git checkout -b $argv
+    end
+end
 
 alias conf='/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME'
