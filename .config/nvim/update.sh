372773e217ce749102fe7f9923b6b7bd0e7f5895
012e <huyphmnhat@gmail.com>
2022-08-19 17:05:09 +0700

    nvim/update.sh: use /bin/sh

--- .config/nvim/update.sh
+++ .config/nvim/update.sh
@@ -1,29 +1,36 @@
-#! /usr/bin/fish
-set -l temp /tmp/NvChad
-set -l dest ~/.config/nvim
+#! /usr/bin/sh
+temp=/tmp/NvChad
+dest=~/.config/nvim
 
+# sync fork
+echo "syncing the fork"
+gh repo sync 012e/nvchad -b main
+
+# download fork
 rm -rf /tmp/NvChad
-git clone https://github.com/NvChad/NvChad /tmp/NvChad
+echo "cloning the fork"
+gh repo clone 012e/nvchad /tmp/NvChad
+
+# copy files
 cp $temp/init.lua $dest/ -r
-# cp $temp/.stylua.toml $dest/ -r
+cp $temp/.stylua.toml $dest/ -r
 cp $temp/lua/core $dest/lua/ -r
 cp $temp/lua/plugins $dest/lua/ -r
 
-set -l old_commit_hash (cat ./commit_hash.txt)
-set -l current_commit_hash (git --git-dir=$temp/.git rev-parse HEAD)
-if [ $old_commit_hash != $current_commit_hash ]
+old_commit_hash=$(cat ./commit_hash.txt)
+current_commit_hash=$(git --git-dir=$temp/.git rev-parse HEAD)
 
-    # handle new comits
-    git --git-dir=$temp/.git log \
-                  $old_commit_hash..$current_commit_hash \
-                > changelog.txt
+if [ "$old_commit_hash" != "$current_commit_hash" ]; then
+	# handle new commits
+	git --git-dir=$temp/.git log \
+		"$old_commit_hash".."$current_commit_hash" \
+		>changelog.txt
 
-    # update old commit hash to newer one
-    echo $current_commit_hash > ./commit_hash.txt
+	# update old commit hash to newer one
+	echo "$current_commit_hash" >./commit_hash.txt
 else
-    echo "not updated" > ./changelog.txt
-end
+	echo "not updated" >./changelog.txt
+fi
 
 # update packer_compile
 nvim -c PackerSync
-
