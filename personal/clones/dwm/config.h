6477c8b827cf5388041ce7c089de475ebf8cdf88
012e <huyphmnhat@gmail.com>
2022-08-24 22:20:20 +0700

    dwm: config.h customization

--- personal/clones/dwm/config.h
+++ personal/clones/dwm/config.h
@@ -10,17 +10,17 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;     /* 0 means no bar */
 static const int topbar             = 1;     /* 0 means bottom bar */
-static const char *fonts[]          = { "JetbrainsMono Nerd Font:size=12" };
+static const char *fonts[]          = { "JetbrainsMono Nerd Font:size=13.5" };
 static const char dmenufont[]       = "JetbrainsMono Nerd Font:size=24";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_gray1[]       = "#282828";
+static const char col_gray2[]       = "#504945";
+static const char col_gray3[]       = "#bdae93";
+static const char col_gray4[]       = "#ebdbb2";
+static const char col_cyan[]        = "#cc241d";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  "#ff0000"  },
+	[SchemeSel]  = { col_gray4, col_gray2,  col_gray1  },
 };
 
 /* tagging */
@@ -35,6 +35,7 @@ static const Rule rules[] = {
 	{ "Pavucontrol",    NULL,       NULL,       0,            1,           -1,        50,50,500,300,        3 },
 	{ "Thunar",         NULL,       NULL,       0,            1,           -1,        50,50,1000,1000,      3 },
 	{ "Google-chrome",  NULL,       NULL,       1 << 0,       0,           -1,        50,50,500,300,        3 },
+	{ "firefox",        NULL,       NULL,       1 << 0,       0,           -1,        50,50,500,300,        3 },
 };
 
 
@@ -65,7 +66,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *roficmd[] = { "rofi", "-show", "drun", NULL };
+static const char *roficmd[] = { "rofi", "-show", "run", NULL };
 static const char *termcmd[]  = { "sttmux", NULL };
 
 // Volume controls
