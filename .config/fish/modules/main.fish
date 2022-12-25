dc1ff6972c1881377261b6e02698e32ab4a06a0c
012e <huyphmnhat@gmail.com>
2022-12-25 13:02:08 +0700

    fish(misc): minor improvement

--- .config/fish/modules/main.fish
+++ .config/fish/modules/main.fish
@@ -2,11 +2,14 @@ source $HOME/.config/fish/modules/utils.fish
 
 fish_default_key_bindings
 
+set -l config_path "$HOME/.config"
+
 # ENV vars
 _env_var GOPATH "$HOME/.local/share/go/"
 _env_var EDITOR nvim
 _env_var MANPAGER "sh -c 'col -bx | bat -l man -p'"
-_env_var XDG_DOWNLOAD_DIR "$HOME/files"
+_env_var XDG_DOWNLOAD_DIR "$HOME/down"
+_env_var GIT_CONFIG_GLOBAL "$config_path/git/config"
 
 # Dependencies
 _import exa pacman syspower keybindings nvim fzf git z fishtheme
@@ -20,4 +23,5 @@ _alias rip "killall"
 _alias ... "cd ../../"
 _alias .... "cd ../../../"
 _alias pyenv "source ./venv/bin/activate.fish"
+alias rm "trash-put"
 trap "begin tmux kill-session -t $fish_pid; tmux kill-session -t popup$fish_pid; end &> /dev/null" EXIT
