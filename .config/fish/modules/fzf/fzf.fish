901edbc3539b16509b577bc5560a8fef96169f9d
012e <huyphmnhat@gmail.com>
2022-12-25 13:01:35 +0700

    fish: rename fzf functions

--- .config/fish/modules/fzf/fzf.fish
+++ .config/fish/modules/fzf/fzf.fish
@@ -1,32 +1,27 @@
-function _fzf_find_and_cd
+function cf
     set -l dest (fd --type d | fzf)
     if [ "$dest" != "" ]
         cd $dest
     end
 end
 
-function _fzf_find_hidden_and_cd
+function cf.
     set -l dest (fd -H --type d | fzf)
     if [ "$dest" != "" ]
         cd $dest
     end
 end
 
-function _fzf_find_files
+function ff
     set -l dest (dirname (fd | fzf))
     if [ "$dest" != "" ]
         cd $dest
     end
 end
 
-function _fzf_find_hidden_files
+function ff.
     set -l dest (dirname (fd -H | fzf))
     if [ "$dest" != "" ]
         cd $dest
     end
 end
-
-_alias cf _fzf_find_and_cd
-_alias cf. _fzf_find_hidden_and_cd
-_alias ff _fzf_find_files
-_alias ff. _fzf_find_hidden_files
